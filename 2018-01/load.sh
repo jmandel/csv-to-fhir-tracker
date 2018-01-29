0c6954ad87b9d8e9ed7f3864d59d1f84e84a1cc4
Josh Mandel <jmandel@gmail.com>
2018-01-29 01:09:29 -0600

    Loaded immunizations

---
+++ 2018-01/load.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+../chromedriver &
+#export GFORGE_USERNAME=fhir_bot
+#export GFORGE_PASSWORD=REDACTED
+
+#python ../drive-gforge.py --generate-select-options
+#exit 0
+
+
+#python ../drive-gforge.py --tracker 677 --csvfile core.csv --slug core --do-creates --do-updates
+#python ../drive-gforge.py --tracker 677 --csvfile immunization.csv --slug immunizations --do-creates --do-updates
+
+kill %1

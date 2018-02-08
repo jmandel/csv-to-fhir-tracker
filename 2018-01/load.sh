32bb4ee9fd0a31df17600658a6ef25f829ac59d2
Josh Mandel <jmandel@gmail.com>
2018-02-07 20:42:24 -0600

    loaded vhd
    loaded vhd

--- 2018-01/load.sh
+++ 2018-01/load.sh
@@ -11,6 +11,9 @@
 #python ../drive-gforge.py --tracker 677 --csvfile core.csv --slug core --do-creates --do-updates
 #python ../drive-gforge.py --tracker 677 --csvfile immunization.csv --slug immunizations --do-creates --do-updates
 #python ../drive-gforge.py --tracker 677 --csvfile qicore.csv --slug qicore --do-creates --do-updates
-python ../drive-gforge.py --tracker 677 --csvfile uscore.csv --slug uscore --do-creates --do-updates
+#python ../drive-gforge.py --tracker 677 --csvfile uscore.csv --slug uscore --do-creates --do-updates
+#python ../drive-gforge.py --tracker 677 --csvfile vhd.csv --slug vhd --do-creates --do-updates
+
+#python ../drive-gforge.py --tracker 677 --csvfile ecr.csv --slug eCR --do-creates --do-updates
 
 kill %1

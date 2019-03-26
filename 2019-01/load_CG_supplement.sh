4ca3b1499264d5224e9f6a9e9a26b6d360afdff0
Josh Mandel <jmandel@med.harvard.edu>
2019-03-26 09:21:05 -0700

    Added CG Jan 2019 supplement

---
+++ 2019-01/load_CG_supplement.sh
@@ -0,0 +1,9 @@
+
+python ../drive-gforge.py \
+    --tracker 677 \
+    --csvfile "documents/ballots/2019-01 IGs/preload-csvs/FHIR_IG_CG_R1_D2_2019JAN_Supplement.csv" \
+    --slug "CG" \
+    --do-updates \
+    --do-creates \
+    --logfile progress-CG.json
+
